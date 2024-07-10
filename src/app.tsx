8feae18f000c895ed6039f4c40c73f98c9851852
max <me@mclifford.dev>
2024-07-10 19:54:31 +0100

    Work on app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -10,97 +10,23 @@ import {createRoot} from "react-dom/client";
 import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "./components/ui/tooltip";
 import {Sheet, SheetContent, SheetTrigger} from "./components/ui/sheet";
 import {Button} from "./components/ui/button";
+import {Card} from "./components/ui/card";
+import {ResizableHandle, ResizablePanel, ResizablePanelGroup} from "./components/ui/resizable";
+import {Explorer} from "./components/explorer";
 
 const root = createRoot(document.getElementById('root'));
 
+//const layout = cookies().get("react-resizable-panels:layout")
+//const collapsed = cookies().get("react-resizable-panels:collapsed")
+
+//const defaultLayout = layout ? JSON.parse(layout.value) : undefined
+//const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined
+
+
 root.render(
     <React.StrictMode>
-        <div className="flex min-h-screen w-full flex-col bg-muted/40">
-
-            <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-                <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-                    <a href="#"
-                       className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base">
-                        <Package2 className="h-4 w-4 transition-all group-hover:scale-110"/>
-                        <span className="sr-only">Acme Inc</span>
-                    </a>
-                    <TooltipProvider>
-                        <Tooltip>
-                            <TooltipTrigger asChild>
-                                <a href="#"
-                                   className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8">
-                                    <Home className="h-5 w-5"/>
-                                    <span className="sr-only">Dashboard</span>
-                                </a>
-                            </TooltipTrigger>
-                            <TooltipContent side="right">Queues</TooltipContent>
-                        </Tooltip>
-                    </TooltipProvider>
-                </nav>
-            </aside>
-            <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-                <Sheet>
-                    <SheetTrigger asChild>
-                        <Button size="icon" variant="outline" className="sm:hidden">
-                            <PanelLeft className="h-5 w-5"/>
-                            <span className="sr-only">Toggle Menu</span>
-                        </Button>
-                    </SheetTrigger>
-                    <SheetContent side="left" className="sm:max-w-xs">
-                        <nav className="grid gap-6 text-lg font-medium">
-                            <Link
-                                href="#"
-                                className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
-                            >
-                                <Package2 className="h-5 w-5 transition-all group-hover:scale-110"/>
-                                <span className="sr-only">Acme Inc</span>
-                            </Link>
-                            <Link
-                                href="#"
-                                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                            >
-                                <Home className="h-5 w-5"/>
-                                Dashboard
-                            </Link>
-                            <Link
-                                href="#"
-                                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                            >
-                                <ShoppingCart className="h-5 w-5"/>
-                                Orders
-                            </Link>
-                            <Link
-                                href="#"
-                                className="flex items-center gap-4 px-2.5 text-foreground"
-                            >
-                                <Package className="h-5 w-5"/>
-                                Products
-                            </Link>
-                            <Link
-                                href="#"
-                                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                            >
-                                <Users2 className="h-5 w-5"/>
-                                Customers
-                            </Link>
-                            <Link
-                                href="#"
-                                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-                            >
-                                <LineChart className="h-5 w-5"/>
-                                Settings
-                            </Link>
-                        </nav>
-                    </SheetContent>
-                </Sheet>
-
-                <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-                    <div className="mx-auto grid max-w-[59rem] flex-1 auto-rows-max gap-4">
-                        <h1>header</h1>
-                    </div>
-
-                </main>
-            </div>
+        <div className="h-dvh w-full">
+            <Explorer topics={[]} defaultLayout={[265, 440, 655]} navCollapsedSize={4} />
         </div>
     </React.StrictMode>
 )
