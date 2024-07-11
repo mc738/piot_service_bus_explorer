412c80abcc31d56072ba163af9e45eefe8573ad2
max <me@mclifford.dev>
2024-07-11 20:59:58 +0100

    Work on json-viewer.tsx

--- src/components/json-viewer.tsx
+++ src/components/json-viewer.tsx
@@ -4,42 +4,42 @@ interface JsonViewerProps {
     data: object
 }
 
-function StringProperty({key, value}: StringPropertyProps) {
-    return <div><span className="font-bold">{key}</span>: "{value}"</div>;
+function StringProperty({stringPropertyKey, value}: StringPropertyProps) {
+    return <div><span className="font-bold">{stringPropertyKey}</span>: "{value}"</div>;
 }
 
 type StringPropertyProps = {
-    key: string
+    stringPropertyKey: string
     value: string
 }
 
-function NumberProperty({key, value}: NumberPropertyProps) {
-    return <div><span className="font-bold">{key}</span>: {value}</div>;
+function NumberProperty({numberPropertyKey, value}: NumberPropertyProps) {
+    return <div><span className="font-bold">{numberPropertyKey}</span>: {value}</div>;
 }
 
 type NumberPropertyProps = {
-    key: string
+    numberPropertyKey: string
     value: number
 }
 
-function BoolProperty({key, value}: BoolPropertyProps) {
-    return <div><span className="font-bold">{key}</span>: {value ? 'true' : 'false'}</div>;
+function BoolProperty({boolPropertyKey, value}: BoolPropertyProps) {
+    return <div><span className="font-bold">{boolPropertyKey}</span>: {value ? 'true' : 'false'}</div>;
 }
 
 type BoolPropertyProps = {
-    key: string
+    boolPropertyKey: string
     value: boolean
 }
 
-function UndefinedProperty({key}: UndefinedPropertyProps) {
-    return <div><span className="font-bold">{key}</span>: <span className="text-muted italic">undefined</span></div>;
+function UndefinedProperty({undefinedPropertyKey}: UndefinedPropertyProps) {
+    return <div><span className="font-bold">{undefinedPropertyKey}</span>: <span className="text-muted italic">undefined</span></div>;
 }
 
 type UndefinedPropertyProps = {
-    key: string
+    undefinedPropertyKey: string
 }
 
-function NodeItem({key, value}: NodeItemProps) {
+function PropertyItem({propertyKey, value}: NodeItemProps) {
 
     if (typeof value === "object") {
         if (value.constructor === Object) {
@@ -48,21 +48,20 @@ function NodeItem({key, value}: NodeItemProps) {
             return <div></div>;
         }
     } else if (typeof value === "string") {
-        console.log(key);
-        return <StringProperty key={key} value={value as unknown as string}/>;
+        return <StringProperty stringPropertyKey={propertyKey} value={value as unknown as string}/>;
     } else if (typeof value === "number") {
-        return <NumberProperty key={key} value={value as unknown as number}/>;
+        return <NumberProperty numberPropertyKey={propertyKey} value={value as unknown as number}/>;
     } else if (typeof value === "boolean") {
-        return <BoolProperty key={key} value={value as unknown as boolean}/>;
+        return <BoolProperty boolPropertyKey={propertyKey} value={value as unknown as boolean}/>;
     } else if (typeof value === "undefined") {
-        return <UndefinedProperty key={key}/>;
+        return <UndefinedProperty undefinedPropertyKey={propertyKey}/>;
     } else {
         return <div></div>
     }
 }
 
 type NodeItemProps = {
-    key: string
+    propertyKey: string
     value: any
 }
 
@@ -71,10 +70,10 @@ export function JsonViewer({data}: JsonViewerProps) {
     const d = data as any;
 
     return (
-        <div>
+        <div className="font-mono">
             {
-                Object.keys(d).map(key => {
-                    return <NodeItem key={key} value={d[key]}/>;
+                Object.keys(d).map((key, i) => {
+                    return <PropertyItem key={i} propertyKey={key} value={d[key]}/>;
                 })
             }
         </div>
