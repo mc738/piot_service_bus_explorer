2e15a5dd5854a75a3df46aea64388a55cd6b1660
max <me@mclifford.dev>
2024-07-11 21:30:13 +0100

    Work on message_display.tsx

--- src/components/message_display.tsx
+++ src/components/message_display.tsx
@@ -4,10 +4,58 @@ import {Tooltip, TooltipContent, TooltipTrigger} from "./ui/tooltip";
 import {Button} from "./ui/button";
 import {Archive, Reply} from "lucide-react";
 import {Separator} from "./ui/separator";
+import {JSONTree} from "react-json-tree";
+import {JsonViewer} from "./json-viewer";
 
 interface MessageDisplayProps {
     message: MessageItem | null
 }
+
+const theme = {
+    scheme: 'monokai',
+    author: 'wimer hazenberg (http://www.monokai.nl)',
+    base00: '#272822',
+    base01: '#383830',
+    base02: '#49483e',
+    base03: '#75715e',
+    base04: '#a59f85',
+    base05: '#f8f8f2',
+    base06: '#f5f4f1',
+    base07: '#f9f8f5',
+    base08: '#f92672',
+    base09: '#fd971f',
+    base0A: '#f4bf75',
+    base0B: '#a6e22e',
+    base0C: '#a1efe4',
+    base0D: '#66d9ef',
+    base0E: '#ae81ff',
+    base0F: '#cc6633',
+};
+
+const data = {
+    name: "name",
+    description: "description",
+    date: Date(),
+    data: {
+      id: "id",
+      name: "data name",
+      user: "user_1"
+    },
+    items: [
+        {
+            id: "item_1",
+            name: "Item 1",
+            description: "This is item 1",
+        },
+        {
+            id: "item_2",
+            name: "Item 2",
+            description: "This is item 2",
+        }
+    ]
+}
+
+
 
 export function MessageDisplay({message}: MessageDisplayProps) {
     const today = Date()
@@ -46,6 +94,30 @@ export function MessageDisplay({message}: MessageDisplayProps) {
                     <div className="flex items-start p-4">
                         <div className="flex items-start gap-4 text-sm">
                             <h1>{message.name}</h1>
+
+                            <div className="flex-1 whitespace-pre-wrap p-4 text-sm">
+                                <JsonViewer data={data} />
+
+
+                                <JSONTree
+                                    data={data}
+                                    theme={{
+                                        extend: theme,
+                                        // underline keys for literal values
+                                        valueLabel: {
+                                            textDecoration: 'underline',
+                                        },
+                                        // switch key for objects to uppercase when object is expanded.
+                                        // `nestedNodeLabel` receives additional argument `expandable`
+                                        nestedNodeLabel: ({ style }, keyPath, nodeType, expanded) => ({
+                                            style: {
+                                                ...style,
+                                                textTransform: expanded ? 'uppercase' : style.textTransform,
+                                            },
+                                        }),
+                                    }}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
